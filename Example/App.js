74adc006556066787a22e3fe45b978029a87eddf
nanndoj <nanndoj@gmail.com>
2018-08-03 13:09:03 +0200

    More organized example

--- Example/App.js
+++ Example/App.js
@@ -7,59 +7,21 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Image,
-  Button
-} from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
-import { Emitter } from 'react-native-particles';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-const { width, height } = Dimensions.get('window');
+import CoinsExplosion from './examples/CoinsExplosion';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Emitter
-          autoStart={false}
-          numberOfParticles={1000}
-          emissionRate={20}
-          interval={750}
-          particleLife={1000}
-          direction={-90}
-          spread={360}
-          width={width}
-          height={height}
-          segments={20}
-          speed={8}
-          gravity={0.2}
-          fromPosition={() => ({ x: width / 2 - 50, y: height / 2 - 160 })}
-          style={{ position: 'absolute', top: 0, left: 0, zIndex: 999 }}
-          ref={emitter => (this.emitter = emitter)}
-        >
-          <Image
-            source={require('./assets/star.png')}
-            resizeMode="stretch"
-            style={{ width: 100, height: 100 }}
-          />
-        </Emitter>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Button title={'Click me'} onPress={() => this.emitter.start()} />
+        <CoinsExplosion ref={emitter => (this.coinsExplosion = emitter)} />
+
+        <Button
+          title={'Coins explosion'}
+          onPress={() => this.coinsExplosion.start()}
+        />
       </View>
     );
   }
@@ -71,15 +33,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
   }
 });
---
+++ Example/examples/CoinsExplosion.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Dimensions, Image } from 'react-native';
+
+import { Emitter } from 'react-native-particles';
+
+const { width, height } = Dimensions.get('window');
+
+type Props = {};
+export default class CoinsExplosion extends Component<Props> {
+  render() {
+    const { ref } = this.props;
+    return (
+      <Emitter
+        autoStart={true}
+        numberOfParticles={50}
+        interval={200}
+        emissionRate={25}
+        particleLife={3000}
+        direction={-90}
+        spread={150}
+        speed={8}
+        segments={100}
+        width={width}
+        height={height}
+        fromPosition={{ x: width / 2, y: height / 2 }}
+        style={styles.emitter}
+        gravity={0.2}
+        ref={emitter => (this.emitter = emitter)}
+      >
+        <Image
+          style={styles.coin}
+          source={require('../assets/coin.png')}
+          resizeMode="stretch"
+        />
+      </Emitter>
+    );
+  }
+
+  start() {
+    this.emitter.start();
+  }
+
+  stopEmitting() {
+    this.emitter.stopEmitting();
+  }
+}
+
+const styles = StyleSheet.create({
+  emitter: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0
+  },
+  coin: {
+    width: 24,
+    height: 24
+  }
+});
